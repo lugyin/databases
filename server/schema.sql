74f0f6e743e6d89dc9aa9cac86b8080587c94a80
lugyin <lugyin@gmail.com>
2015-04-17 17:36:46 -0700

    Finished populating chat database;

--- server/schema.sql
+++ server/schema.sql
@@ -2,11 +2,33 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+CREATE TABLE `messages` (
+  `index` INT NOT NULL AUTO_INCREMENT,
+  `username` VARCHAR(100),
+  `roomname` VARCHAR(100),
+  `createat` DATETIME,
+  `text` VARCHAR(200),
+  PRIMARY KEY  (`index`)
 );
 
-/* Create other tables and define schemas for them here! */
+CREATE TABLE `users` (
+  `username` VARCHAR(100) NOT NULL,
+  `friends` VARCHAR(100),
+  PRIMARY KEY  (`username`)
+);
+
+CREATE TABLE `rooms` (
+  `roomname` VARCHAR(100) NOT NULL,
+  `username` VARCHAR(100),
+  PRIMARY KEY  (`roomname`)
+);
+
+ALTER TABLE `messages` ADD CONSTRAINT `messages_fk1` FOREIGN KEY (`username`) REFERENCES users(`username`);
+ALTER TABLE `messages` ADD CONSTRAINT `messages_fk2` FOREIGN KEY (`roomname`) REFERENCES rooms(`roomname`);
+
+ALTER TABLE `rooms` ADD CONSTRAINT `rooms_fk1` FOREIGN KEY (`username`) REFERENCES users(`username`);
+
+
 
 
 
